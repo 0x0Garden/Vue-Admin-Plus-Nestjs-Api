c126fcee7c9a5ad5188e340d82ad2b2efd3dc08f
Jaxson Wang <i@iiong.com>
2021-02-24 22:16:04 +0800

    feat:add global prefix api

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
 
   const config = new DocumentBuilder()
     .setTitle('Vue Admin Plus 管理系统接口文档')
